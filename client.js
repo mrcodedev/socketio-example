0d2e066cbe399473b9bd97972811f64483d9ca7b
José Luis García <joseluis.garcia@altafonte.com>
2021-08-22 20:48:28 +0200

    chore: little project to how use socket.io

--- client.js
+++ client.js
@@ -2,25 +2,30 @@ const io = require("socket.io-client");
 
 const socket = io("ws://0.0.0.0:3000", { autoconect: true });
 
+// When the server is connected
 socket.on("connect", () => {
   console.log("Socket-io server connected :D");
-  socket.send("TE ENVIO ESTO");
+  socket.send("I connect to the server :D");
 });
 
+// When receive emit with "random" name
 socket.on("random", (message) => {
   console.log("ramdom:", message);
 });
 
+// When receive with send method
 socket.on("message", (message) => {
   console.log("message", message);
 });
 
+// When the server is disconnected
 socket.on("disconnect", () => {
   if (socket.disconnected) {
     console.log("Socket-io server disconnected:(");
   }
 });
 
+// Testing sending message
 setTimeout(() => {
   socket.emit("random", { message: "Send random message" });
 }, 5000);
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server);
 
+// When user is connected to server
 io.on("connection", (socket) => {
   console.log("User connected :D");
 
-  //Using
+  //Using name event
   socket.on("random", (message) => {
     socket.broadcast.emit("random", message);
   });
 
-  // Using .send
+  // Using default event "message"
   socket.on("message", (message) => {
     console.log("message", message);
   });
 
+  // When client is disconnect
   socket.on("disconnect", () => {
     console.log("User disconnected :(");
   });
 });
 
+// Starting Server
 server.listen({ port: 3000, server: "0.0.0.0.0" }, () => {
   console.log("Server is listenning in 0.0.0.0:3000");
 });
